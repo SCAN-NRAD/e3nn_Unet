34d0e0e05f336bdc2b4fc377ba59d958b7f0b614
Ivan Diaz <ivan.diaz@skeletoncad.com>
2022-07-12 14:22:27 +0200

    fix import error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='e3nn_Unet',
-    version='0.1.0',
+    version='0.1.1',
     description='Segmentation network with O3 and SO3 equivariant convolutions',
     url='https://github.com/SCAN-NRAD/e3nn_Unet',
     classifiers=[
@@ -12,8 +12,8 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     keywords='MRI, segmentation',
-    packages=['e3nn_Unet'],
-    package_dir={'e3nn_Unet': './e3nn_Unet'},
+    package_dir={'': 'e3nn_Unet'},
+    packages=find_packages(where='e3nn_Unet'),
     python_requires='>=3.7',
     install_requires=['e3nn==0.3.5',
                       'nnunet==1.6.5',
